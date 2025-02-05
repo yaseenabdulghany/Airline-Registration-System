ad1f0fc2f221a1b05d2e11288c129dd1228db842
Yaseen Abdulghany <62149387+yaseenabdulghany@users.noreply.github.com>
2025-02-05 16:14:03 +0200

    Update Main.java

--- src/Airline/Main.java
+++ src/Airline/Main.java
@@ -1,17 +1,37 @@
 package Airline;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Passenger {
+    public int passenger_id;
+    public String passenger_name;
+    public String passenger_email;
+    public String passenger_mobile;
+    public String passenger_password;
+    public String passenger_username;
+    public String passenger_address;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public Passenger(int passenger_id, String passenger_name, String passenger_email, String passenger_mobile, String passenger_password, String passenger_username, String passenger_address) {
+        this.passenger_id = passenger_id;
+        this.passenger_name = passenger_name;
+        this.passenger_email = passenger_email;
+        this.passenger_mobile = passenger_mobile;
+        this.passenger_password = passenger_password;
+        this.passenger_username = passenger_username;
+        this.passenger_address = passenger_address;
     }
+
+    public void addPassenger(){
+        System.out.println("Passenger added: " + passenger_name);
+    }
+    public void editPassenger() {
+        System.out.println("Passenger edited: " + passenger_name);
+    }
+    public void deletePassenger() {
+        System.out.println("Passenger deleted: " + passenger_name);
+
+    }
+    public void searchPassenger() {
+        System.out.println("searching for passenger: " + passenger_name);
+
+    }
+
 }
